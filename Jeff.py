2df26c11caaae4e8e175e15e5421a96991e67d01
bidoofis <bidoofis.official@gmail.com>
2025-03-31 11:10:55 -0500

    added level up messages and changed level up odds. new danbo powered response, and new liberal propoganda.

--- Jeff.py
+++ Jeff.py
@@ -16,25 +16,32 @@ class Jeff(discord.Client):
         if "You're welcome :)" in message.content:
             await message.reply("<:mahirobruh:1155998284876353617>") 
 
-        # If the message if from the bot don't give a shit
+        # If the message is from the bot don't give a shit
         if message.author.bot: 
             return
-        # 1/1000 chance for chance
-        chance = 0.001
+        # 1/500 chance for chance
+        chance1000 = 0.002
+
+        # i'm getting CRAZY with this 1/5,000 chance!!
+        chance10k = 0.0002
 
         # random.random() generates a number between 0 and 1.0 
         # 0.001 is just 1/1000, so that's the chance it rrepresents., therefore we check if the number is less than to get that chance.
-        if random.random() < chance:
+        if random.random() < chance1000:
             await message.channel.send("The liberals would not approve of this.")
 
-        # Second verse same as the first
-        chanceLvl = 0.003333
+        # guess bro.
+        if random.random() < chance10k:
+            await message.channel.send("The liberals WOULD approve of this!")
+
+        # Third verse same as the first
+        chanceLvl = 0.002
 
         # Define categories, choose from list, store level up message, do!!!!!
         if random.random() < chanceLvl:
-            category1 = random.choice(["Mustache Points", "Epicness", "Gamer Skill", "Ls recieved"])
-            category2 = random.choice(["Hours of Anime Watched", "Backflips accomplished", "Toes collected","Tweets ratio'd", "BKZ twitter accounts reported"])
-            category3 = random.choice(["Embarassment points", "Persona slots gained", "Bogos binted", "is a number"])
+            category1 = random.choice(["Mustache Points", "Epicness", "Gamer Skill", "Ls recieved", "False Statements"])
+            category2 = random.choice(["Hours of Anime Watched", "Backflips accomplished", "Toes collected","Tweets ratio'd", "BKZ twitter accounts reported", "Media Literacy Credits"])
+            category3 = random.choice(["Embarassment points", "Persona slots gained", "Bogos binted", "is a number", "ignore the number, this is a cry for help"])
             randomNum1 = int(random.random() * 100 * (1 if random.random() < 0.8 else -1))
             randomNum2 = int(random.random() * 100 * (1 if random.random() < 0.8 else -1))
             randomNum3 = int(random.random() * 100 * (1 if random.random() < 0.8 else -1))
@@ -79,6 +86,9 @@ class Jeff(discord.Client):
             await message.reply("It's in the pudding.")
             return
         
+        if "danbo" in message.content.lower():
+            await message.channel.send("https://media.discordapp.net/attachments/678770209527693322/1349571889663840307/danbo2.gif?ex=67ebfa04&is=67eaa884&hm=b69ff7a9de48a50784e3ecf4ab549b4f06103e4af9b18f50407a80c487ad717c&=&width=1096&height=1402")
+            return
 
     # This shit not bussin' bussin' on god fr
     #async def on_reaction_add(reaction, user, member):
